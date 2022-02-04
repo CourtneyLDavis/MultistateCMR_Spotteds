2664a76c30ea109eaccd6cbde67d02e42a5417a2
Courtney Davis <cld74@cornell.edu>
2022-02-03 22:43:43 -0500

    add files via upload

---
+++ Scripts/BOOTSTRAPS_jDist_fullmodel_M.R
@@ -0,0 +1,104 @@
+###############################################
+###############################################
+# Code for model implemented in:
+# Davis, C.L., D.J. Muñoz, S.M. Amburgey, C.R. Dinsmore, E.W. Teitsworth, and D.A.W. Miller
+# "Multistate mark-recapture model to estimate sex-specific dispersal rates and distances for a wetland breeding amphibian metapopulation"
+
+
+# THIS HEAVILY BORROWS FROM CODE PUBLISHED IN:
+# Worthington, H., R. McCrea, R. King and R. Griffiths. (2019).
+# "Estimating abundance from multiple sampling capture-recapture data via a multi-state multi-period stopover model"
+# The Annals of Applied Statistics, 13(4): 2043-2064.
+# https://doi.org/10.1214/19-AOAS1264
+
+###############################################
+###############################################
+
+# -------------------------------------------------------------------------------
+# This script defines the function to bootstrap the capture histories, 
+# perform the optimization of the HMM likelihood and store the parameter 
+# estimates at each bootstrap iteration.
+# -------------------------------------------------------------------------------
+
+# -------------------------------------------------------------------------------
+# Inputs: param - vector of parameters to be passed to param.unpack
+#         n - number of observed individuals
+#         T - number of years
+#         K - number of occasions in each year
+#         X - list of capture histories
+#         Z - attendance history
+#         n.hist - frequency of histories
+#         distSq - matrix describing distances between sites
+#         nboot - the number of bootstraps to perform
+# Outputs: matrix of parameters estimates
+#          !!! WRITES FILES OUT AFTER EACH BOOTSTRAP !!!      
+# -------------------------------------------------------------------------------
+
+bootstrap <- function(param, n, T, K, X, Z, n.hist, distSq, nboot)  {
+  
+  # indicator of number of successful bootstraps
+  flag <- 0
+  
+  # storage matrix
+  res <- matrix(0, nrow=nboot, ncol=length(param))
+  
+  # names for files
+  Znames <- rep(0, nboot)
+  Xnames <- matrix(0, nrow=nboot, ncol=T)
+  for (i in 1:nboot)  {
+    Znames[i] <- paste('Zboot', i, '.txt', sep='')
+    for (t in 1:T)  {
+      Xnames[i,t] <- paste('Xboot', i, 'year', t, '.txt', sep='')
+    }
+  }
+  
+  # to perform the bootstrap...
+  while (flag < nboot)  {
+    
+    # bootstrap data by sampling individuals
+    bootsample <- sample(1:n, n, replace=T)
+    Z.boot <- Z[bootsample,]
+    X.boot <- NULL
+    for (t in 1:T)  {
+      X.boot[[t]] <- X[[t]][bootsample,]
+    }
+    
+    write.table(Z.boot, Znames[flag+1], row.names=F, col.names=F)
+    for (t in 1:T)  {
+      write.table(X.boot[[t]], Xnames[(flag+1),t], row.names=F, col.names=F)
+    }
+    
+    # run optimiser to maximise HMM likelihood
+    opt <- nlm(likelihood, param, n, T, K, X.boot, Z.boot, n.hist, distSq, iterlim = 500)
+    if (opt$code == 1 | opt$code == 2)  {
+      flag2 <- 3
+    } else  {
+      flag2 <- 1
+    }
+    
+    # check for convergence
+    while (flag2 != 3)  {
+      opt <- nlm(likelihood, opt$estimate, n, T, K, X.boot, Z.boot, n.hist, distSq, iterlim = 500)
+      if (opt$code == 1 | opt$code == 2)  {
+        flag2 <- 3
+      } else  {
+        flag2 <- flag2+1
+      }
+    }
+    
+    # store estimates
+    res[flag+1,] <- opt$estimate
+    
+    # increase bootstrap number
+    if (flag2 == 3)  {
+      flag <- flag+1
+    }
+    
+    # print and write to file
+    print(c('bootstrap', flag, 'finished'))
+    write.table(res, 'bootstrapres.txt', row.names=F, col.names=F)
+  }
+  
+  # results
+  return(res)
+}
---
+++ Scripts/RUN_jDist_primarymove_sec_unobs_F.R
@@ -0,0 +1,79 @@
+###############################################
+###############################################
+# Code for model implemented in:
+# Davis, C.L., D.J. Muñoz, S.M. Amburgey, C.R. Dinsmore, E.W. Teitsworth, and D.A.W. Miller
+# "Multistate mark-recapture model to estimate sex-specific dispersal rates and distances for a wetland breeding amphibian metapopulation"
+
+
+# THIS HEAVILY BORROWS FROM CODE PUBLISHED IN:
+# Worthington, H., R. McCrea, R. King and R. Griffiths. (2019).
+# "Estimating abundance from multiple sampling capture-recapture data via a multi-state multi-period stopover model"
+# The Annals of Applied Statistics, 13(4): 2043-2064.
+# https://doi.org/10.1214/19-AOAS1264
+
+###############################################
+###############################################
+
+# read in script file with model functions
+source('Scripts/FUNCTIONS_jDist_primarymove_sec_unobs_F.R')
+
+# read in data for primary level (between-year observations)
+Z <- read.table('Data/attendance_12ponds_primarymove_females.txt', header=F)
+
+# read in data for secondary level (within-year observations)
+X <- NULL
+X[[1]] <- read.table('Data/state2014_females.txt', header=F)
+X[[2]] <- read.table('Data/state2015_females.txt', header=F)
+X[[3]] <- read.table('Data/state2016_females.txt', header=F)
+X[[4]] <- read.table('Data/state2017_females.txt', header=F)
+X[[5]] <- read.table('Data/state2018_females.txt', header=F)
+X[[6]] <- read.table('Data/state2019_females.txt', header=F)
+
+# read in distance matrix and convert to kilometers
+distSq <- read.csv("Data/DistanceMatrix_AMMACMR.csv", header = F)
+colnames(distSq) <- NULL
+distSq <- as.matrix(distSq)
+distSq <- distSq/1000
+
+# set constants
+n <- length(X[[1]][,1])
+T <- 6
+K <- rep(0, T)
+for (t in 1:T)  {
+  K[t] <- length(X[[t]][1,])
+}
+n.hist <- rep(1,n) # data contain no repeat histories
+
+# set initial values for all parameters
+param <- c(2.0914, 
+           0.11346734, -1.386294, -0.4054651, -0.8472979, -0.67278778, 
+           1.47823538, 
+           0.92719425, 
+           -0.93515200, -1.72337910, -0.91372267, -4.04938570, -1.26881758, -2.67502349,
+           rep(-1.098012, 11),
+           rep(1.47823538,6),
+           -1.09,
+           -1.50819940, -10.34739280, -2.04477353, -1.72637293, -13.12783719, -1.47074036,
+           -1.40, 
+           -1.09,
+           2.00,
+           2.00)
+
+# check the vector is unpacked correctly
+params <- param.unpack(param, n, T, K, distSq)
+
+# try likelihood function at starting values
+lik <- likelihood(param, n, T, K, X, Z, n.hist, distSq)
+
+# optimise likelihood
+opt <- nlm(likelihood, param, n, T, K, X, Z, n.hist, distSq, iterlim = 500)
+
+# MLEs (using output from nlm)
+res <- param.unpack(opt$estimate, n, T, K, distSq)
+
+
+## Run to calculate bootstrap 95% confidence intervals
+source('BOOTSTRAPS_jDist_primarymove_sec_unobs_F.R')
+
+# run bootstrap (using output from nlm)
+resboot <- bootstrap(opt$estimate, n, T, K, X, Z, n.hist, distSq, 50)
